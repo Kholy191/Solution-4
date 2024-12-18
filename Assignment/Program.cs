ac84d31a079140ba0dd0e52b96808c7c3ca142a0
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-18 21:19:24 +0200

    Q13

---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q13 Write a program to allow the user to enter a string and print the REVERSE of it.
+            //string str;
+            //str = Console.ReadLine() ?? "Default value";
+
+            //StringBuilder Rstr;
+            //Rstr = new StringBuilder();
+
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    Rstr.Append(str[str.Length - i - 1]);
+            //}
+
+            //Console.WriteLine(Rstr);
+            #endregion
+        }
+    }
+}
