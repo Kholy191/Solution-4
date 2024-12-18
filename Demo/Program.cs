366ba2c39cbbc5c7c5bc41e2dcea090ff778ded7
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-18 11:46:55 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object Name1 = new object();
+            Name1 = "Ahmed";
+            object Name2 = new object();
+            Name2 = "Mahmoud";
+
+            Name2 = Name1;
+
+            Console.WriteLine(Name1);
+            Console.WriteLine(Name1.GetHashCode());
+            Console.WriteLine(Name2);
+            Console.WriteLine(Name2.GetHashCode());
+            Console.WriteLine("__________");
+
+            Name1 = "omara";
+            Console.WriteLine(Name1);
+            Console.WriteLine(Name1.GetHashCode());
+            Console.WriteLine(Name2);
+            Console.WriteLine(Name2.GetHashCode());
+            Console.WriteLine("__________")
+        }
+    }
+}
