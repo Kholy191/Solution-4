f3fa24a925ac954ff2896101d46cf4bdb3b4bc24
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-18 21:22:25 +0200

    Q15

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -40,6 +40,37 @@ namespace Assignment
 
             #endregion
 
+            #region Q15 Write a program in C# Sharp to find prime numbers within a range of numbers.
+            //int StartNum = 0;
+            //int EndNum = 0;
+            //Console.WriteLine("Input starting number of range:");
+            //int.TryParse(Console.ReadLine(), out StartNum);
+            //Console.WriteLine("Input ending number of range :");
+            //int.TryParse(Console.ReadLine(), out EndNum);
+
+            //for (int Num = StartNum; Num <= EndNum; Num++)
+            //{
+            //    Boolean Flag = true;
+            //    for (int i = 2; i < Num; i++)
+            //    {
+            //        if ((Num % i) == 0)
+            //        {
+            //            Flag = false;
+            //            break;
+            //        }
+
+            //        else
+            //        {
+            //            Flag = true;
+            //        }
+            //    }
+            //    if (Flag == true && Num != 0 && Num != 1)
+            //    {
+            //        Console.WriteLine(Num);
+            //    }
+            //}
+            #endregion
+
         }
     }
 }
